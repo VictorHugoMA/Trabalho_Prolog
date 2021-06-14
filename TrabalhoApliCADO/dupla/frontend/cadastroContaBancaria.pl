65e682d92e67ec521a4ea09dd107f0c2e953a656
PeSantanna <pedro.santana@ufu.br>
2021-06-14 13:05:39 -0300

    Realocação

---
+++ TrabalhoApliCADO/dupla/frontend/cadastroContaBancaria.pl
@@ -0,0 +1,30 @@
+/* html//1, reply_html_page  */
+:- use_module(library(http/html_write)).
+/* html_requires  */
+:- use_module(library(http/html_head)).
+
+:- ensure_loaded(gabarito(boot5rest)).
+
+contabancaria(_Pedido):-
+    reply_html_page(
+        boot5rest,
+        [ title('Cadastro Conta Bancaria')],
+        [ div(class(container),
+              [ \html_requires(js('bookmark.js')),
+                h1('Insira os dados para cadastro'),
+                \form_contabancaria
+              ]) ]).
+
+form_contabancaria -->
+    html(form([ id('bookmark-form'),
+                onsubmit("redirecionaResposta( event, '/' )"),
+                action('/api/v1/cadastroContaBancaria/') ],
+              [ \metodo_de_envio('POST'),
+                    \campo(usuario,'Usuario:',text),
+                    \campo(nome,'Nome:',text),
+                    \campo(senha,'Senha:',text),
+                    \campo(confirmaSenha,'Corfirmar Senha:',text),
+                \enviar_ou_cancelar('/')
+              ])).
+
+
