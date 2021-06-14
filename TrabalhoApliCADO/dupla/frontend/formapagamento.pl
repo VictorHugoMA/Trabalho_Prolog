e11535d9ab2fe8feea90bc28051b06ad4c5fbe08
VictorHugoMA <victor.alves1@ufu.br>
2021-06-14 12:11:31 -0300

    add form forma pagamento

--- TrabalhoApliCADO/dupla/frontend/formapagamento.pl
+++ TrabalhoApliCADO/dupla/frontend/formapagamento.pl
@@ -7,36 +7,19 @@
 
 formapagamento(_Pedido):-
     reply_html_page(
-            bootstrap5,
-            [ title('Formas de Pagamentos')],
-              form([ class(container),action='/receptorFor', method='POST'],
-                [ \html_requires(css('estilo.css')),
-                    h2(class("my-5 text-center"),
-                        'Formas de Pagamentos'),
-                    \campo(descr_formapagento, 'Descricao', text),
-          
-
-                    p(button([class('btn btn-primary'), type(submit)],'Cadastrar')),
-                    \retorna_home  ])).
-
-
-
-:- http_handler('/receptorFor', recebe_FormaPag(Method),
-            [ method(Method),
-                methods([post]) ]).
-
-recebe_FormaPag(post,Pedido) :-
-        catch(
-            http_parameters(Pedido,[descr_formapagento(Descr_formapagento,[])]),
-            _E,
-            fail),
-        !,
-        tabFormaPag:insere(_Id_formapagamento, Descr_formapagento),
-        reply_html_page(
-            bootstrap,
-            [ title('Pedido')],
+            boot5rest,
+            [ title('Cadastro Forma de Pagamento')],
             [ div(class(container),
-                [ h1('Pedido Recebido'),
-                    \retorna_home
-                ])
-            ]).
+              [ \html_requires(js('bookmark.js')),
+                h1('Insira os dados para cadastro'),
+                \form_formapagamento
+              ]) ]).
+
+form_formapagamento -->
+    html(form([ id('bookmark-form'),
+                onsubmit("redirecionaResposta( event, '/' )"),
+                action('/api/v1/formapagamento/') ],
+              [ \metodo_de_envio('POST'),
+                \campo(descr_formapagento, 'Descricao', text),
+                \enviar_ou_cancelar('/')
+              ])).
