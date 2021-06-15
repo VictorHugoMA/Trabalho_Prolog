55e6db4efcdcdf784dba2d3dfc10896177bf52b6
PeSantanna <pedro.santana@ufu.br>
2021-06-15 12:50:15 -0300

    A

--- TrabalhoApliCADO/dupla/backend/bd/tabelas/tbl_formapagamento.pl
+++ TrabalhoApliCADO/dupla/backend/bd/tabelas/tbl_formapagamento.pl
@@ -1,4 +1,5 @@
-created(1623771648.402367).
+created(1623772113.188413).
 assert(formapagamento(3,"cavalo")).
 assert(formapagamento(4,"cavalo")).
 assert(formapagamento(5,"Cartao")).
+assert(formapagamento(6,"Cartao")).
