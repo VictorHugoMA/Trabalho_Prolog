be2d03f07e2028becee92cdce92e01cb8f0b82c2
PeSantanna <pedro.santana@ufu.br>
2021-06-16 11:43:43 -0300

    Tabelas

--- TrabalhoApliCADO/dupla/backend/bd/tabelas/tbl_empresa.pl
+++ TrabalhoApliCADO/dupla/backend/bd/tabelas/tbl_empresa.pl
@@ -1,3 +1,3 @@
-created(1623801449.405845).
+created(1623854613.095278).
 assert(empresa(1,"Mercearia do Cleber","Servidor","Juridica","23462346","1111111","22222222","Rua das Orquideas","Arvoredo","Uberaba","36754511","MG","4331156","cas","Cleber","6666777889","Dono")).
 assert(empresa(2,"Mercearia do Cleber","Servidor2","Juridica","23462346","1111111","22222222","Rua das Orquideas","Santo Gabriel","Uberlandia","36754511","MG","4331156","asqdz","Cleber","6666777889","Dono")).
--- TrabalhoApliCADO/dupla/backend/bd/tabelas/tbl_formapagamento.pl
+++ TrabalhoApliCADO/dupla/backend/bd/tabelas/tbl_formapagamento.pl
@@ -1,4 +1,4 @@
-created(1623801449.403346).
+created(1623854613.092776).
 assert(formapagamento(3,"cavalo")).
 assert(formapagamento(4,"cavalo")).
 assert(formapagamento(5,"Cartao")).
